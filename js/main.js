d7a7abf33a91f57484ef4120f40f98f5ed923abe
Loretapiap <loretapiap@gmail.com>
2016-07-15 15:24:58 -0400

    todo app

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var texto = document.getElementById("inputCuadro").value;
+var lista = document.getElementById("lista");
+var listaTareas = document.getElementById("listaTareas");
+
+function agregarTarea() {
+    var texto = document.getElementById("inputCuadro").value;
+   
+    if(texto != ""){
+    	var div = document.createElement("div");
+    	div.setAttribute("class", "contenedor");
+    	var checked = document.createElement("input");
+    	checked.setAttribute("type", "checkbox");
+    	checked.setAttribute("class", "checked");
+        var tarea = document.createElement("span");
+        tarea.setAttribute("class", "tarea");
+        var btn = document.createElement("button");
+        btn.setAttribute("class", "glyphicon glyphicon-trash");
+        btn.addEventListener('click', eliminarTarea);
+        checked.onclick = function(){
+        	if(this.checked === true){
+        		tarea.setAttribute("class", "tachar");
+        	}else{
+        		tarea.removeAttribute("class");
+        	}
+        }
+        listaTareas.appendChild(div);
+        div.appendChild(checked);
+        tarea.innerHTML = texto;
+        div.appendChild(tarea);
+        div.appendChild(btn);
+        document.getElementById("inputCuadro").value = "";
+    }else{
+    	alert("Por favor, escribe una tarea");
+    }
+}
+
+function eliminarTarea(e) {
+ 	var div = e.target.parentNode;
+ 	listaTareas.removeChild(div);   
+}
